fb99851f79b146e5e375bc4488e643ad407efc52
Yatin_Singh <yatinsingh331@gmail.com>
2025-01-01 16:38:43 +0530

    added update comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,8 +1,8 @@
 import { Blog } from "../models/blog.models.js";
 import { Comment } from "../models/comment.models.js";
 import { User } from "../models/user.models.js";
-import ApiError from "../utils/ApiError";
-import ApiResponse from "../utils/ApiResponse";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const createComment = asyncHandler(async(req,res)=>{
@@ -34,3 +34,42 @@ const createComment = asyncHandler(async(req,res)=>{
     console.log("commented : ",populatedComment);
     return res.status(200).json(new ApiResponse(200,populatedComment,"Commented on the blog successfully"))
 })
+
+const updateComment = asyncHandler(async(req, res)=>{
+    const {blogId, commentId} = req.params
+    const {content} = req.body
+    const userId = req.user._id
+    if(!content?.trim()){
+        throw new ApiError(400,"Content not found to post")
+    }
+    // const oldComment = await Comment.findOne({
+    //     _id:commentId,
+    //     owner:userId,
+    //     blog:blogId
+    // })
+    
+    // if(!oldComment) throw new ApiError(400,"Blog not found")
+    
+    const comment = await Comment.findByIdAndUpdate(
+        // i did not know that u can pass an object instead of only _id
+        {
+            _id: commentId,
+            owner:userId
+        },
+        {
+            $set:{
+                content:content
+            }
+        },{
+            new : true,
+            runValidators:true
+        }
+    ).populate("(owner","username email")
+
+    if(!comment){
+        throw new ApiError(404,"Comment not found or you are not authorized to update")
+    }
+    
+    return res.status(200).json(new ApiResponse(200,comment,"Comment updated successfully!"))
+})
+export {createComment, updateComment}
